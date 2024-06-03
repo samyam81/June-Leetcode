3b3d43cc54febe8b4d9c20b17258fb938fa7c476
samyam81 <samyam081@gmail.com>
2024-06-03 12:55:48 +0545

    Append Character

---
+++ June3/AppendCharacter.java
@@ -0,0 +1,14 @@
+public class AppendCharacter {
+    public int appendCharacters(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (s.charAt(i) == t.charAt(j)) j++;
+            i++;
+        }
+        
+        return n - j;
+    }
+}
