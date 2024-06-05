e1a0f4f4d9873f09e1571c08d03eb8c7e46df495
samyam81 <samyam081@gmail.com>
2024-06-05 08:41:58 +0545

    June 5 ReadME

--- June4/LongestPalindrome.java
+++ June4/LongestPalindrome.java
@@ -5,25 +5,26 @@ import java.util.HashMap;
 public class LongestPalindrome {
     public int longestPalindrome(String s) {
 
-        if(s.isEmpty()) return 0;
-        HashMap<Character, Integer> myHashMap=new HashMap<>();
+        if (s.isEmpty()) return 0;
+        HashMap<Character, Integer> myHashMap = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            char c=s.charAt(i);
-            if (myHashMap.containsKey(c)) 
-                myHashMap.put(c,myHashMap.get(c)+1); 
-            else   
-                myHashMap.put(c, 1);                      
+            char c = s.charAt(i);
+            if (myHashMap.containsKey(c))
+                myHashMap.put(c, myHashMap.get(c) + 1);
+            else
+                myHashMap.put(c, 1);
         }
 
-        int s81=0;
-        boolean kbd=false;
+        int s81 = 0;
+        boolean kbd = false;
 
-        for (Character ks11 : myHashMap.keySet()) 
-            if (myHashMap.get(ks11)%2==0) s81=s81+myHashMap.get(ks11);
-            else{
+        for (Character ks11 : myHashMap.keySet())
+            if (myHashMap.get(ks11) % 2 == 0)
                 s81 = s81 + myHashMap.get(ks11);
-                if (!kbd)  kbd=true;
-                 else s81=s81-1; 
+            else {
+                s81 = s81 + myHashMap.get(ks11);
+                if (!kbd) kbd = true;
+                else s81 = s81 - 1;
             }
         return s81;
 
---
+++ June5/ReadMe.md
@@ -0,0 +1,16 @@
+# Find Common Characters: June 5
+
+## Intuition
+To find the characters that appear in all strings within the input array, we can use a hashmap to count the characters in the first word, then iterate through the rest of the words to update the counts accordingly.
+
+## Approach
+1. Initialize a hashmap to store the count of characters in the first word.
+2. Iterate through the characters in the first word and update the count in the hashmap.
+3. Iterate through the rest of the words.
+4. For each word, create a new hashmap to count the characters.
+5. Update the counts in the main hashmap with the minimum counts from the current word.
+6. Construct the result array based on the characters that appear in all words.
+
+## Complexity
+- Time complexity: $$O(n \cdot m)$$, where $$n$$ is the number of words in the input array and $$m$$ is the maximum length of a word. We iterate through each character in each word.
+- Space complexity: $$O(k)$$, where $$k$$ is the number of unique characters that appear in all words. We store the character counts in the hashmap.
